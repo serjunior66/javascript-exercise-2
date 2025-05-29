e79e0ebb16ae41bde0667736b3eaa926815d2863
serjunior66 <kabongoserjunior@gmail.com>
2025-05-29 06:43:01 -0700

    completed exercise 2

---
+++ exercise2.js
@@ -0,0 +1,22 @@
+// TODO: Create a variable called number1 and store any number in it
+let number1 = 60
+console.log(number1);
+
+// TODO: Create a variable called number2 and store a different number in it
+let number2 = 17
+console.log(number2);
+
+// TODO: Convert number1 to a string and store it in a new variable
+number1 = ('this is not a drill')
+console.log(number1);
+
+// TODO: Add number1 and number2 and store the result in a variable called sum
+number1 = 60
+let sum = [number1 + number2]
+console.log(sum);
+
+// TODO: Multiply number1 and number2 and store the result in a variable called product
+let product = [number1 * number2]
+console.log(product);
+
+// TODO: Print all results to the console
