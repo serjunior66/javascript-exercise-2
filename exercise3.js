24a9b078600c09ae138df9cdf98a4ca23bfe04a8
serjunior66 <kabongoserjunior@gmail.com>
2025-05-29 07:17:41 -0700

    completed-exercise-3

---
+++ exercise3.js
@@ -0,0 +1,17 @@
+// TODO: Create an empty array called colors
+let color = []
+// TODO: Add three colors to the array using push()
+color.push('red')
+color.push('black')
+color.push('green')
+// TODO: Create another array called numbers with five numbers
+let numbers = [1, 2, 3, 4, 5]
+// TODO: Remove the last color from the colors array
+color.pop();
+// TODO: Add a new color to the beginning of the colors array
+color.unshift('purple')
+// TODO: Print the length of both arrays
+console.log(color);
+console.log(numbers);
+
+
